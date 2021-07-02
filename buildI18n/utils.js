7b118a3b363befcdeb134517bc405b26c7f867fd
Sathish Kumar <83972575+REV0310@users.noreply.github.com>
2021-07-01 20:59:42 -0500

    Adding js actions

---
+++ buildI18n/utils.js
@@ -0,0 +1,33 @@
+module.exports = function() { 
+    this.log = function(msg) {
+	  console.log(msg); 
+	};
+	
+	this.run = function(cmd, cwd = null) {
+		return execSync(cmd, { encoding: "utf8", stdio: "inherit", cwd });
+	}
+	
+	this.getInput = function(name, required = false) {
+		const value = getEnv(`INPUT_${name.toUpperCase()}`);
+		if (value == null) {
+			// Value is either not set (`undefined`) or set to `null`
+			if (required) {
+				throw new Error(`"${name}" input variable is not defined`);
+			}
+			return null;
+		}
+		return value;
+    }
+}
+
+
+function run(cmd, cwd = null) {
+	return execSync(cmd, { encoding: "utf8", stdio: "inherit", cwd });
+}
+
+
+function getEnv(name) {
+	return process.env[name];
+}
+
+
