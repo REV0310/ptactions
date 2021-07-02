edec2febdef22bac371edf4880d35108baa69db7
Sathish Kumar <83972575+REV0310@users.noreply.github.com>
2021-07-01 21:06:04 -0500

    Adding js actions

--- utils.js
+++ utils.js
@@ -1,24 +1,25 @@
-module.exports = function() { 
-    this.log = function(msg) {
-	  console.log(msg); 
-	};
-	
-	this.run = function(cmd, cwd = null) {
-		return execSync(cmd, { encoding: "utf8", stdio: "inherit", cwd });
-	}
-	
-	this.getInput = function(name, required = false) {
-		const value = getEnv(`INPUT_${name.toUpperCase()}`);
-		if (value == null) {
-			// Value is either not set (`undefined`) or set to `null`
-			if (required) {
-				throw new Error(`"${name}" input variable is not defined`);
-			}
-			return null;
-		}
-		return value;
-    }
+
+const log = function(msg) {
+  console.log(msg); 
+};
+
+const run = function(cmd, cwd = null) {
+	return execSync(cmd, { encoding: "utf8", stdio: "inherit", cwd });
 }
 
+const getInput = function(name, required = false) {
+	const value = getEnv(`INPUT_${name.toUpperCase()}`);
+	if (value == null) {
+		// Value is either not set (`undefined`) or set to `null`
+		if (required) {
+			throw new Error(`"${name}" input variable is not defined`);
+		}
+		return null;
+	}
+	return value;
+}
 
+module.exports = function() { 
+ log
+}
 
