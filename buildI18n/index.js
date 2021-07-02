b6faeba90013bd7b8d88e238b98dba107607da13
Sathish Kumar <83972575+REV0310@users.noreply.github.com>
2021-07-01 20:38:28 -0500

    Adding js actions

--- buildI18n/index.js
+++ buildI18n/index.js
@@ -1,34 +1,7 @@
 const { execSync } = require("child_process");
 const { unlinkSync, writeFileSync } = require("fs");
 const path = require("path");
-
-
-function log(msg) {
-	console.log(msg); // eslint-disable-line no-console
-}
-
-
-function run(cmd, cwd = null) {
-	return execSync(cmd, { encoding: "utf8", stdio: "inherit", cwd });
-}
-
-
-function getEnv(name) {
-	return process.env[name];
-}
-
-
-function getInput(name, required = false) {
-	const value = getEnv(`INPUT_${name.toUpperCase()}`);
-	if (value == null) {
-		// Value is either not set (`undefined`) or set to `null`
-		if (required) {
-			throw new Error(`"${name}" input variable is not defined`);
-		}
-		return null;
-	}
-	return value;
-}
+const tools = require('./tools');
 
 
 function runAction() {
