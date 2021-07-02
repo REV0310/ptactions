e14ecd12dce4e2a51e9b2ef33f3e596c7a9f45ea
Sathish Kumar <83972575+REV0310@users.noreply.github.com>
2021-07-01 20:55:19 -0500

    Adding js actions

--- utils.js
+++ utils.js
@@ -1,5 +1,23 @@
-function log(msg) {
-	console.log(msg); // eslint-disable-line no-console
+module.exports = function() { 
+    this.log = function(msg) {
+	  console.log(msg); 
+	};
+	
+	this.run = function(cmd, cwd = null) {
+		return execSync(cmd, { encoding: "utf8", stdio: "inherit", cwd });
+	}
+	
+	this.getInput = function(name, required = false) {
+		const value = getEnv(`INPUT_${name.toUpperCase()}`);
+		if (value == null) {
+			// Value is either not set (`undefined`) or set to `null`
+			if (required) {
+				throw new Error(`"${name}" input variable is not defined`);
+			}
+			return null;
+		}
+		return value;
+    }
 }
 
 
@@ -13,14 +31,3 @@ function getEnv(name) {
 }
 
 
-function getInput(name, required = false) {
-	const value = getEnv(`INPUT_${name.toUpperCase()}`);
-	if (value == null) {
-		// Value is either not set (`undefined`) or set to `null`
-		if (required) {
-			throw new Error(`"${name}" input variable is not defined`);
-		}
-		return null;
-	}
-	return value;
-}
